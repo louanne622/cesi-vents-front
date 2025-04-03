4c61f93ad1c18be789c7aa286c76532edffe4ee5
Alexis COUPAS <alexis.coupas@viacesi.fr>
2025-04-03 09:37:07 +0200

    fix: color page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function LoginPage() {
   return (
     <div className="login-page bg-white">
-      <div className="absolute inset-0 flex items-center justify-center md:ml-50">
+      <div className="absolute  bg-white inset-0 flex items-center justify-center md:ml-50">
         <div className="w-full max-w-md px-4 -mt-16">
           <div className="flex justify-center mb-8">
             <Image
